96f0521fc70de74f8b2f8345c053cab7505f59b8
QuincyClain <79206316+QuincyClain@users.noreply.github.com>
2021-02-23 13:59:09 +0300

    Add files via upload

---
+++ Lab1v26/Lab1v26/Lab1v26.cpp
@@ -0,0 +1,26 @@
+﻿// Lab1v26.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+
+int proverka(int a)
+{
+    int i = 10;
+    while(i < a) 
+    {
+        i = i * 10;
+    }
+    return i;
+}
+ 
+int main()
+{    
+    for(int i = 1; i < 1000; i++)
+    {
+        if(i == (i*i) % proverka(i)) 
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
